d9ededae0b12cee5f5986e3d2b3d9c61d465fa80
Miguel E Uicab Canabal <miguelmort9@gmail.com>
2025-07-19 06:10:33 -0600

    implement user registration API with validation and token generation

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(StoreUserRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $validatedData = $request->validated();
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Usuario creado exitosamente',
+            'user' => $user,
+            'token_type' => 'Bearer',
+            'token' => $token,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('users', App\Http\Controllers\Api\UserController::class)
+    ->only(['store']);
