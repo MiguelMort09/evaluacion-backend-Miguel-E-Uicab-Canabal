d6071923a9b9215294b6f04dd02cad4de4c86ed4
Miguel E Uicab Canabal <miguelmort9@gmail.com>
2025-07-19 09:59:14 -0600

    add product API routes with authentication and throttling middleware

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', App\Http\Controllers\Api\UserController::class)
     ->only(['store']);
+
+// Rutas de documentación
+
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+    Route::apiResource('products', App\Http\Controllers\Api\ProductController::class);
+});
